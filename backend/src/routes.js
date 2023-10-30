d5a2c068381e51524bc85bc663679754f6a2e448
Gustaf Ekström <gusek93@gmail.com>
2023-10-30 13:31:57 +0100

    Created component for displaying elevators with basic styling

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ElevatorManager = require("./elevatorManager");
+const db = require("./database")
 
 const router = express.Router();
 const elevatorManager = new ElevatorManager();
--- elevator-frontend/src/App.js
+++ elevator-frontend/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+
+import Heading from './components/Heading/Heading';
+import ElevatorStatus from './components/ElevatorStatus/ElevatorStatus';
+
 
 const App = () => {
-  return (
-    <div>
 
+  const [elevators, setElevators] = useState([]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:3000/api/elevator/")
+    .then((res) => {
+      const data = res.data;
+      console.log(data);
+      setElevators(data)
+    })
+    .catch(error => console.error("Something went wrong", error));
+  },[]);
+
+  return (
+    <div className='App'>
+      <Heading  headline = {'Elevator App'}/>
+      <ElevatorStatus elevators={elevators} />
     </div>
   )
 }
