4398ffe3754e64ef28b305f5aa8c7b755ab7deb5
Gustaf Ekström <gusek93@gmail.com>
2023-10-30 20:30:12 +0100

    Modified css for call elevator, added input field.

--- elevator-frontend/src/components/CallElevator/CallElevator.js
+++ elevator-frontend/src/components/CallElevator/CallElevator.js
@@ -2,9 +2,20 @@ import React from 'react'
 import "./callElevator.css"
 
 const CallElevator = () => {
+
+  const handleCallElevator = () => {
+    
+  }
+
+
   return (
-    <div className='w-75 call-elevator-container'>
-      <button className="h-75 w-75 btn btn-secondary">Call Elevator</button>
+    <div className='w-75 call-elevator-container d-block'>
+      <h3 className='text-decoration-underline'>Call Elevator</h3>
+      <div className='d-flex input-container'>
+        <input className="form-control w-75"  type="text" />
+        <button className="btn btn-secondary" onClick={handleCallElevator}>Call Elevator</button>
+      </div>
+      
     </div>
   )
 }
